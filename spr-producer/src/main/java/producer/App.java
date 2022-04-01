889b8f9860507ca6c220bcbc7b839cc29b0b7092
Varinder jassal <varinderjassal@Varinders-MacBook-Pro.local>
2022-04-01 13:15:54 -0700

    fixed minor issues

--- spr-producer/src/main/java/producer/App.java
+++ spr-producer/src/main/java/producer/App.java
@@ -1,11 +1,10 @@
-package com.reactivestax;
+package producer;
 
 import com.reactivestax.avro.OrderEvents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import producer.MessagePublisher;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
--- spr-producer/src/main/java/producer/MessagePublisher.java
+++ spr-producer/src/main/java/producer/MessagePublisher.java
@@ -5,14 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
 public class MessagePublisher {
 
-    @Value("${kafka.topic}")
-    private String topic;
+//    @Value("${kafka.topic}")
+//    private String topic;
 
     @Autowired
     private KafkaTemplate kafkaTemplate;
